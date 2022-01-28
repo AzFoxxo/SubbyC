1f65ff9a3598171c2596c9347ece2d66570def94
AzTheFox/KnotMasterAz <63619429+KnotMasterAz@users.noreply.github.com>
2022-01-28 22:32:32 +0000

    Added SubbyC header file

---
+++ subbyc.h
@@ -0,0 +1,33 @@
+// Requires stdio and stdbool
+#include <stdio.h>
+#include <stdbool.h>
+
+// Lets abuse the C Preprocessor
+#define yip printf
+#define yap puts
+#define ish ==
+#define is =
+#define knot !=
+#define nu !
+#define gibs return
+#define knots return
+#define top true
+#define bottom false
+#define fox bottom
+#define verse 0.5
+#define nowothing null
+#define darkness void
+#define uwu ;
+#define owo ;
+#define an &&
+#define owor ||
+#define yiffy if
+#define big double
+#define small float
+#define inty int
+#define bowol bool
+#define wowrds char[]
+#define notouchi const
+#define dom main
+#define owopen {
+#define clowose }
