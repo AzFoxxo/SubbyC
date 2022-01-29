9abbd1dbe763b0b5f6a44da3994ce922b7fe3c18
Az Foxxo <KnotMasterAz@users.noreply.github.com>
2022-01-29 00:02:45 +0000

    Restructuring project

--- examples/calc.c
+++ examples/calc.c
@@ -1,48 +1,15 @@
-// Requires stdio and stdbool
-#include <stdio.h>
-#include <stdbool.h>
-
-// Lets abuse the C Preprocessor
-#define yip printf
-#define yap puts
-#define ish ==
-#define is =
-#define knot !=
-#define nu !
-#define gibs return
-#define knots return
-#define top true
-#define bottom false
-#define fox bottom
-#define verse switch
-#define switch 0.5
-#define nowothing null
-#define darkness void
-#define uwu ;
-#define owo ;
-#define an &&
-#define owor ||
-#define yiffy if
-#define big double
-#define small float
-#define inty int
-#define bowol bool
-#define wowrds char[]
-#define notouchi const
-#define dom main
-#define owopen {
-#define clowose }
+// SubbyC
+#include "../subbyc.h"
 
 // program
 inty dom(darkness)
-owopen
+        owopen
     big num1 is 0 uwu
     big num2 is 0 uwu
 
     char op is '_' uwu
 
     small results is 0 uwu
-
 
     // nums one
     yip("Nums one: ") uwu
@@ -57,29 +24,27 @@ owopen
     scanf("%s", &op) uwu
 
     // calculates the results
-    yiffy (op ish '+')
+    yiffy(op ish '+')
     owopen
         results is num1 + num2 uwu
-    clowose
-    else yiffy (op ish '-')
+    clowose else yiffy(op ish '-')
     owopen
         results is num1 - num2 uwu
     clowose
-    else yiffy (op ish '/')
+        else yiffy(op ish '/')
     owopen
         results is num1 / num2 uwu
     clowose
-    else
+        else
     owopen
-        results is num1 * num2 uwu
+        results is num1 *num2 uwu
     clowose
 
     // shows results
     yap("------------------------------------------------------------------") uwu
     yip("Results ish %f", results) uwu
-    yip("\n") uwu
-    yap("------------------------------------------------------------------") uwu
-
+    yip("\n") uwu yap("------------------------------------------------------------------") uwu
+    
     // returns 0
     knots bottom owo
 clowose
---
+++ examples/hewwo.c
@@ -0,0 +1,18 @@
+// SubbyC
+#include "../subbyc.h"
+
+// program
+inty dom(darkness)
+    owopen
+    // create a bool called great and set it to true
+    notouchi bowol greet is top uwu
+
+    // if greet is true, print greeting
+    yiffy(greet knot bottom)
+        owopen
+    yap("Hewwo, Worlds!") uwu
+    clowose
+
+    // returns 0
+    knots bottom owo
+clowose
