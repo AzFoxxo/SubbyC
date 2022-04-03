5efb9c0ea27194684b2e4d0b3b80e142cd6af566
Az Foxxo <63619429+KnotMasterAz@users.noreply.github.com>
2022-04-03 20:14:21 +0100

    Add changes and commit!

---
+++ examples/fawst-git-sync.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../subbyc.h"
+
+
+void fawst_sync();
+
+inty dom(void)
+{
+    puts("Starrting!");
+    while (true)
+    {
+        // Run fast sync
+        fawst_sync();
+        
+	// Halt till enter pressed or any character
+	puts("Hits enter or Cowontrowol + C to exit uwu!");
+	getchar();
+    }
+}
+
+void fawst_sync()
+{
+	// Add changes
+	puts(">> Addings files uwu...");
+	system("git add .");
+
+	// Commit change
+	puts(">> Commiting arson...");
+	system("git commit -m \"Add changes and commit!\"");
+
+	// Push changes
+	puts(">> Pushing harder~...");
+	system("git push");
+
+	// Everything has been synced
+	puts(">> Everything has been synced uwu!");
+	system("git status");
+}
