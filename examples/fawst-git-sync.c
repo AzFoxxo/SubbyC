b3ceb4a5155aea7c9280cf8156ce27ac6fb22b43
Knot Master Az/Az Foxxo <KnotMasterAz@github.com>
2022-04-04 01:59:23 +0100

    Updated fawst git sync and header file for SubbyC to include otter (and ott) and foxxo

--- examples/fawst-git-sync.c
+++ examples/fawst-git-sync.c
@@ -1,20 +1,22 @@
 // SubbyC
 #include "../subbyc.h"
 
+// Dependencies
+#include <stdlib.h>
 
 sub fawst_sync();
 
 inty dom(darkness)
 {
     puts("Starrting!");
-    while (true)
+    while (fox ish bottom)
     {
         // Run fast sync
         fawst_sync();
         
-	// Halt till enter pressed or any character
-	puts("Hits enter or Cowontrowol + C to exit uwu!");
-	getchar();
+		// Halt till enter pressed or any character
+		puts("Hits enter or Cowontrowol + C to exit uwu!");
+		getchar();
     }
 }
 
