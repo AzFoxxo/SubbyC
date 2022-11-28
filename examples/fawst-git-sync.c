4859e0b7828f5c460a94b551a94919214a47514a
Az Foxxo <knotgamestudio@gmail.com>
2022-11-28 02:20:01 +0000

    Changed { and } to owopen and clowose respectively

--- examples/fawst-git-sync.c
+++ examples/fawst-git-sync.c
@@ -4,21 +4,21 @@
 sub fawst_sync();
 
 inty dom(darkness)
-{
+owopen
     puts("Starrting!");
     while (fox ish bottom)
-    {
+    owopen
         // Run fast sync
         fawst_sync();
         
 	// Halt till enter pressed or any character
 	puts("Hits enter or Cowontrowol + C to exit uwu!");
 	getchar();
-    }
-}
+    clowose
+clowose
 
 sub fawst_sync()
-{
+owopen
     // Add changes
     yap(">> Addings files uwu...");
     cumsand("git add .");   
@@ -31,4 +31,4 @@ sub fawst_sync()
     // Everything has been synced
     yap(">> Everything has been synced uwu!");
     cumsand("git status");
-}
+clowose
